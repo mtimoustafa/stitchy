a43eb26505e18595c425f6531c9f3597cd791ad8
Mohamed Moustafa <mti.moustafa@gmail.com>
2022-03-26 10:49:57 +0100

    Boilerplate yay

---
+++ src/App.svelte
@@ -0,0 +1,40 @@
+<main
+  class="
+    flex
+    flex-col
+    md:flex-row
+    justify-center
+    items-start
+
+    mx-auto
+    max-w-md
+    md:max-w-none
+
+    px-10
+    py-16
+    sm:py-20
+    md:py-24
+    lg:py-36
+
+    space-y-8
+    md:space-y-0
+    md:space-x-16
+
+    text-sky-50
+  "
+>
+  <About />
+</main>
+
+<script>
+  import About from './app/components/Counter.svelte'
+
+  // <body> style must be added dynamically to index.html
+  // for Tailwind to pick it up
+  document.body.classList.add('bg-midnight')
+</script>
+
+<style>
+  @import './main.css';
+</style>
+
---
+++ tailwind.config.cjs
@@ -0,0 +1,17 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  content: ['./src/**/*.{html,js,svelte}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['"Source Sans Pro"', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        'coral': '#a2fddd',
+        'midnight': '#001219',
+      },
+    },
+  },
+  plugins: [],
+}
