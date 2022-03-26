30fc05f7913610be9996e51c6617b046b506420c
Mohamed Moustafa <mti.moustafa@gmail.com>
2022-03-26 16:14:56 +0100

    A really crappy update with a row counter yeet

--- src/app/components/Layout.svelte
+++ src/app/components/Layout.svelte
@@ -47,6 +47,19 @@
         class="block"
       >
 
+      {#each newProject.steps as newStep}
+        <input
+          bind:value={newStep.description}
+          class="block"
+        >
+      {/each}
+      <button
+        class="px-6 py-3 bg-sky-800 text-sky-50 rounded-lg"
+        on:click={() => addStep(newProject)}
+      >
+        Add Step
+      </button>
+
       <button
         class="px-6 py-3 bg-sky-800 text-sky-50 rounded-lg"
         on:click={createProject}
@@ -57,7 +70,40 @@
   {/if}
 
   {#if viewState === 'showProject'}
-    <h2 class="text-xl sm:text-2xl font-bold text-coral">{currentProject.name}</h2>
+    <div class="flex justify-between">
+      <h2 class="text-xl sm:text-2xl font-bold text-coral">{currentProject.name}</h2>
+
+      <button
+        class="px-6 py-3 bg-sky-800 text-sky-50 rounded-lg"
+        on:click={deleteProject(currentProject.id)}
+      >
+        Delete
+      </button>
+    </div>
+
+    <h3 class="text-md sm:text-lg font-bold text-coral">Row Counter</h3>
+    <p class="text-sky-50">{currentProject.rowCount}</p>
+    <button
+      class="px-6 py-3 bg-sky-800 text-sky-50 rounded-lg"
+      on:click={decreaseRowCount}
+    >
+      -
+    </button>
+    <button
+      class="px-6 py-3 bg-sky-800 text-sky-50 rounded-lg"
+      on:click={increaseRowCount}
+    >
+      +
+    </button>
+
+    <h3 class="text-md sm:text-lg font-bold text-coral">Steps</h3>
+    <ul class="text-sky-50">
+      {#each currentProject.steps as step}
+        <li>
+          {step.description}
+        </li>
+      {/each}
+    </ul>
   {/if}
 </main>
 
@@ -74,19 +120,50 @@
       id: uuid(),
       name: 'New Project',
       link: '',
+      steps: [],
+      rowCount: 0,
     }
+    addStep(newProject)
 
     viewState = 'addProject'
   }
 
+  function addStep(project) {
+    project.steps = [...project.steps, {
+      id: uuid(),
+      description: '',
+    }]
+  }
+
   let currentProject
+
+  function increaseRowCount() {
+    projects.map(project => {
+      if (project.id === currentProject.id) {
+        currentProject.rowCount += 1
+        project.rowCount = currentProject.rowCount
+      }
+    })
+    localStorage.setItem('projects', JSON.stringify(projects))
+  }
+
+  function decreaseRowCount() {
+    projects.map(project => {
+      if (project.id === currentProject.id) {
+        currentProject.rowCount = project.rowCount <= 0 ? 0 : project.rowCount - 1
+        project.rowCount = currentProject.rowCount
+      }
+    })
+    localStorage.setItem('projects', JSON.stringify(projects))
+  }
+
   function showProject(id) {
     currentProject = projects.filter(project => project.id === id)[0]
     viewState = 'showProject'
   }
 
   function createProject() {
-    projects.push(newProject)
+    projects = [...projects, newProject]
     localStorage.setItem('projects', JSON.stringify(projects))
 
     viewState = 'projects'
@@ -94,7 +171,10 @@
 
   function deleteProject(id) {
     projects = projects.filter(project => project.id !== id)
+    currentProject = null
     localStorage.setItem('projects', JSON.stringify(projects))
+
+    viewState = 'projects'
   }
 </script>
 
